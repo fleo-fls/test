199da98fe6dba5b4232d4468873f4f2d1a386c16
J-B MULLER <J-B MULLER@DESKTOP-SIGE1AH>
2024-10-06 13:38:17 -0400

    modification du message test

--- src/ClassTest.java
+++ src/ClassTest.java
@@ -3,7 +3,12 @@ public class ClassTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Println("Helloword");
+	}
 
+	private static void Println(String string) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
